4e925c48f2862fab4676c9f44b0ea6f258687aa3
DenisMantow <mantowdenis@gmail.com>
2026-02-23 04:35:53 +0900

    Update RotationAssist.java

--- src/main/java/com/anchorassist/RotationAssist.java
+++ src/main/java/com/anchorassist/RotationAssist.java
@@ -115,7 +115,7 @@ public class RotationAssist {
             hudTimer--;
         } else {
             isWorking = false;
-            microSnapTriggered = false;
+            microSnapTriggered = true;
         }
     }
 
